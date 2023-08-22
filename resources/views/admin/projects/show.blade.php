77aa7b8c7c0d7e7e54a4b75041815edfff6a5313
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-22 15:50:55 +0200

    created show blade, added show CRUD to ProjectController

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="card mb-3">
+        <img src="{{ $project->image_url }}" class="card-img-top" alt=" {{ $project->title }} ">
+        <div class="card-body">
+            <h5 class="card-title">Title: {{ $project->title }} </h5>
+            <p class="card-text">Description: {{ $project->description }} </p>
+            <p class="card-text">GitHub page: {{ $project->link }} </p>
+            <p class="card-text">Date of creation: {{ $project->creation_date }} </p>
+        </div>
+    </div>
+</div>
+@endsection
