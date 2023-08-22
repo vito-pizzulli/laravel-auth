9e5e0d7ec094dbf3ddbab2218dbd1c146593e094
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-22 15:33:49 +0200

    changed 'Projects List' button size & color in admin panel

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,7 +5,7 @@
     <h1 class="mb-5">Admin Panel</h1>
     <h2>Welcome, {{ $user->name }}!</h2>
     <h3 class="mb-5">Your mail is: {{ $user->email }}.</h3>
-    <a class="btn btn-success" href="{{ url('/projects') }}">
+    <a class="btn btn-primary btn-lg" href="{{ url('/projects') }}">
         Projects List
     </a>
 </div>
