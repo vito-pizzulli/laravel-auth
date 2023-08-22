ed7ccf9c264602d56743a7bfb2ea50bd49bd56bb
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-22 15:21:49 +0200

    added button in admin panel that leads to the projects list

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,6 +4,9 @@
 <div class="container">
     <h1 class="mb-5">Admin Panel</h1>
     <h2>Welcome, {{ $user->name }}!</h2>
-    <h3>Your mail is: {{ $user->email }}.</h3>
+    <h3 class="mb-5">Your mail is: {{ $user->email }}.</h3>
+    <a class="btn btn-success" href="{{ url('/projects') }}">
+        Projects List
+    </a>
 </div>
 @endsection
