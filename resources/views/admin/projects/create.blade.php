5186d3826288aad637054552439ec3485d097f0f
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-22 16:35:03 +0200

    created create blade containing the form to insert a new project into the list

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form action="{{ route('admin.projects.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
+        </div>
+        @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+        <div class="mb-3">
+            <label for="link" class="form-label">GitHub page link</label>
+            <input type="text" class="form-control" id="link" name="link">
+        </div>
+        @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+        <div class="mb-3">
+            <label for="creation_date" class="form-label">Date of creation</label>
+            <input type="date" class="form-control" id="creation_date" name="creation_date">
+        </div>
+        @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+        <div class="mb-3">
+            <label for="image_url" class="form-label">Image URL</label>
+            <input type="text" class="form-control" id="image_url" name="image_url">
+            <div id="imageHelp" class="form-text">A preview image for your project.</div>
+        </div>
+        @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+        <button type="submit" class="btn btn-primary">Add New Project</button>
+        <button type="reset" class="btn btn-warning">Reset All Fields</button>
+    </form>
+</div>
+@endsection
