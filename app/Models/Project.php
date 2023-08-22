7c468e74be3759494fce2e1c367cfebdb6e662df
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-22 17:00:52 +0200

    added fillables to Project Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'link',
+        'creation_date',
+        'image_url'
+    ];
 }
