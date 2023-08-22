f302ad12e9560281aa8c9bc5c7a50547910c7d4b
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-22 17:03:33 +0200

    added button in show blade that allows the user to return to the projects list

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,9 +7,12 @@
         <div class="card-body">
             <h5 class="card-title">Title: {{ $project->title }} </h5>
             <p class="card-text">Description: {{ $project->description }} </p>
-            <p class="card-text">GitHub page link: {{ $project->link }} </p>
-            <p class="card-text">Date of creation: {{ $project->creation_date }} </p>
+            <p class="card-text">GitHub Page Link: {{ $project->link }} </p>
+            <p class="card-text">Date Of Creation: {{ $project->creation_date }} </p>
         </div>
     </div>
+    <a class="btn btn-primary" href="{{ url('/projects') }}">
+        Return to Projects List
+    </a>
 </div>
 @endsection
