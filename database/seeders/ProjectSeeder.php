3415666aa703a4ad6b91cc1695178c50b25aa326
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-23 17:24:34 +0200

    added 'slug' to ProjectSeeder, created from id & project title

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -21,6 +21,9 @@ class ProjectSeeder extends Seeder
             $newProject->link = $faker->url();
             $newProject->creation_date = $faker->date();
             $newProject->image_url = $faker->imageUrl(640, 480, 'sites', true);
+            $newProject->slug = $newProject->title;
+            $newProject->save();
+            $newProject->slug = $newProject->id . "-" . $newProject->title;
             $newProject->save();
         }
     }
