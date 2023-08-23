eb3e2129568be4f286720a75a25a09cb0f2d714d
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-23 15:44:12 +0200

    added alert in show blade that informs the user that the project edit was successful, added edit button

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <div class="container">
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
     <div class="card mb-3">
         <img src="{{ $project->image_url }}" class="card-img-top" alt=" {{ $project->title }} ">
         <div class="card-body">
@@ -11,8 +16,7 @@
             <p class="card-text">Date Of Creation: {{ $project->creation_date }} </p>
         </div>
     </div>
-    <a class="btn btn-primary" href="{{ url('/projects') }}">
-        Return to Projects List
-    </a>
+    <a class="btn btn-warning" href="{{route('admin.projects.edit', $project->id)}}">Edit Project</a>
+    <a class="btn btn-secondary" href="{{ url('/projects') }}">Return to Projects List</a>
 </div>
 @endsection
