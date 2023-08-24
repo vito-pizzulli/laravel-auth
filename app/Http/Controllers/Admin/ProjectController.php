0ed296f6754228b544c0529bad2f62efade5df23
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-24 16:53:38 +0200

    changed session message when destroying an instance

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -92,7 +92,7 @@ class ProjectController extends Controller
     public function destroy(Project $project)
     {
         $project->delete();
-        return redirect()->route('admin.projects.index')->with('destroySuccess', 'Project successfully deleted!');
+        return redirect()->route('admin.projects.index')->with('destroySuccess', 'Project successfully moved to the Recycle Bin!');
     }
 
     public function trashed()
